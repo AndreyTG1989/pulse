48551f2bc95ba1030ce1e01e88cd3b2982e3eee8
Andrey Tsigankou <stop-off@yandex.ru>
2021-03-31 15:25:30 +0300

    версия от 31.03.2021 анимация

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
@@ -13,17 +13,17 @@ $(document).ready(function(){
                 }
             }
         ]
-    });
+    }); /*slick*/
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () { /* tabs js*/
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -32,6 +32,82 @@ $(document).ready(function(){
     };
 
     toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
+    toggleSlide('.catalog-item__back');/* tabs js*/
+
+    //modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); /* интерессная функция, подставляет title из html)*/
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    function validateForms(form) {      /*валидация форм*/
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста,введите свое имя",
+                phone: "Пожалуйста,введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста,введите свою почту",
+                    email: "введен не правильный формат почты name@domain.com"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault(); //отменяет стандратное поведение браузера
+        $.ajax({
+            type: "POST", // отдавать данные
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find('input').val("");   // input - теги
+            $('#consultation, #order').fadeOut();  //открытие модальных окон
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset'); // после отправки , очистка
+        });
+        return false;
+    });
+
+
+    //smoth scroll and page up
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+
+new WOW().init();
+
 });
 
